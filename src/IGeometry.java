f5d5f123830390f63b6c6bd3b3493d3da2152aa5
helenStrfanova <http.pug@gmail.com>
2020-06-08 19:33:06 +0300

    Polymorphism

---
+++ src/IGeometry.java
@@ -0,0 +1,18 @@
+/*
+* Interface name : IGeometry
+*
+* Created on 08 June 2020
+*
+* Copyright Alyona Sviridova NTU KhPI
+*
+* Module 2 "Polymorphism" task
+*
+* Create and implement two interfaces for your class.
+*/
+
+public interface IGeometry {
+
+    double getSectorArea();
+    double getPerimeter();
+
+}
---
+++ src/IPackaging.java
@@ -0,0 +1,20 @@
+/*
+* Interface name : IPackaging
+*
+* Created on 08 June 2020
+*
+* Copyright Alyona Sviridova NTU KhPI
+*
+* Module 2 "Polymorphism" task
+*
+* Create and implement two interfaces for your class.
+*/
+
+public interface IPackaging {
+
+    String toString();
+    String toJSON();
+    String toXML();
+    void toConsole();
+
+}
