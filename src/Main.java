bdfc62c31c070b4b45dfa83be152361bf82d33fd
helenStrfanova <http.pug@gmail.com>
2020-06-08 17:09:42 +0300

    Inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+/*
+* Classname : Main
+*
+* Created on 06 June 2020
+*
+* Copyright Alyona Sviridova NTU KhPI
+*
+* */
 public class Main {
 
     public static void main(String[] args) {
@@ -6,5 +14,8 @@ public class Main {
         System.out.println(round1.toString());
         System.out.println(round1.hashCode());
         System.out.println(round1.equals(round1));
+
+        Circle circle1 = new Circle(5,15, 10);
+        System.out.println(circle1.toString());
     }
 }
